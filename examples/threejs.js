9ba756f9c4fe60c83d3660e09e425bcd0f7a301d
Marcel Wiessler <marcel@gaisterhand.de>
2024-05-23 17:05:31 +0200

    update example with remote model

--- examples/threejs.js
+++ examples/threejs.js
@@ -11,13 +11,20 @@ document.body.appendChild(renderer.domElement);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const orbit = new OrbitControls(camera, renderer.domElement);
 
+const grid = new THREE.GridHelper(10, 10);
+scene.add(grid);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(1, 1, 0);
+scene.add(directionalLight);
+
 orbit.target = new THREE.Vector3(0, 0, 0);
-camera.position.x = -30;
-camera.position.y = 20;
-camera.position.z = 30;
+// camera.position.x = -30;
+// camera.position.y = 20;
+camera.position.z = 4;
 
 // This is the model we want to load
-const url = "https://staging.api.cloud.needle.tools/v1/public/90b5411/1659b019/";
+const url = "https://engine.needle.tools/demos/gltf-progressive/threejs/assets/model.glb";
 
 const gltfLoader = new GLTFLoader();
 
@@ -25,9 +32,10 @@ const gltfLoader = new GLTFLoader();
 useNeedleProgressive(url, renderer, gltfLoader)
 
 // just call the load method as usual
-gltfLoader.load(url, gltf => {[
+gltfLoader.load(url, gltf => {
+    console.log(gltf)
     scene.add(gltf.scene)
-]})
+})
 
 
 // Animate the scene

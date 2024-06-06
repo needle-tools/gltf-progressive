4f8879b56f19bdf3b9e52a521d45b1719432915d
Marcel Wiessler <marcel@gaisterhand.de>
2024-06-06 21:01:22 +0200

    setRaycastMesh: debug progressive

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 import { BufferGeometry, Object3D } from "three";
+import { getParam } from "./utils.internal.js";
 
+const debug = getParam("debugprogressive");
 
 /**
  * The raycast mesh is a low poly version of the mesh used for raycasting. It is set when a mesh that has LOD level with more vertices is discovered for the first time
@@ -21,6 +23,7 @@ export function setRaycastMesh(obj: Object3D, geom: BufferGeometry) {
     if (obj.type === "Mesh" || obj.type === "SkinnedMesh") {
         if (!obj.userData) obj.userData = {};
         obj.userData["needle:raycast-mesh"] = geom;
+        if (debug && !geom.getAttribute("position")) console.warn("setRaycastMesh: missing position", geom);
     }
 }
 

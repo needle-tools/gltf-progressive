abf95222a35356004adfa5de1be9322483258304
Marcel Wiessler <marcel@gaisterhand.de>
2024-06-06 16:46:23 +0200

    version, ensure userData exists

--- src/lods_manager.ts
+++ src/lods_manager.ts
@@ -227,6 +227,10 @@ export class LODsManager {
     /** Update the LOD levels for the renderer. */
     private updateLODs(scene: Scene, camera: Camera, object: Mesh, desiredDensity: number, _frame: number) {
 
+        if (!object.userData) {
+            object.userData = {};
+        }
+        
         let state = object.userData.LOD_state as LOD_state;
         if (!state) {
             state = new LOD_state();

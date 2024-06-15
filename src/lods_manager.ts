da6eec91368b73b55f54d00fbfdb3f51c6865cb4
Marcel Wiessler <marcel@gaisterhand.de>
2024-06-15 14:42:29 +0200

    minor log when mesh LOD changes

--- src/lods_manager.ts
+++ src/lods_manager.ts
@@ -584,6 +584,16 @@ export class LODsManager {
             result.mesh_lod = state.lastLodLevel_Mesh;
         }
 
+        if (debugProgressiveLoading) {
+            const changed = result.mesh_lod != state.lastLodLevel_Mesh;
+            if (changed) {
+                const level = mesh_lods?.[result.mesh_lod];
+                if (level) {
+                    console.log(`Mesh LOD changed: ${state.lastLodLevel_Mesh} → ${result.mesh_lod} (${level.density.toFixed(0)}) - ${mesh.name}`);
+                }
+            }
+        }
+
         if (has_texture_lods) {
             // If this is the first time a texture LOD is requested we want to get the highest LOD to not display the minimal resolution that the root glTF contains as long while we wait for loading of e.g. the 8k LOD 0 texture
             if (state.lastLodLevel_Texture < 0) {
@@ -606,7 +616,7 @@ export class LODsManager {
                     if (lod.max_height > pixelSizeOnScreen) {
                         result.texture_lod = i;
                         if (result.texture_lod < state.lastLodLevel_Texture) {
-                            if (debugProgressiveLoading) console.log(`Texture LOD changed ${state.lastLodLevel_Texture} → ${result.texture_lod} (${lod.max_height}px: ${(100 * state.lastScreenCoverage).toFixed(2)} % = ${pixelSizeOnScreen.toFixed(0)}px) - ${mesh.name}`);
+                            if (debugProgressiveLoading) console.log(`Texture LOD changed: ${state.lastLodLevel_Texture} → ${result.texture_lod} (${lod.max_height}px: ${(100 * state.lastScreenCoverage).toFixed(2)} % = ${pixelSizeOnScreen.toFixed(0)}px) - ${mesh.name}`);
                         }
                         break;
                     }
